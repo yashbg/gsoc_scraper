63f17266162ac8864ea74e6f61a9d568fca5c5c7
Yash Gupta <yashbg7@gmail.com>
2021-05-18 19:18:12 +0530

    Update code.py and add gsoc2021.csv

--- code.py
+++ code.py
@@ -1,18 +1,27 @@
+import pandas as pd
 import requests
-import json
 
 url = 'https://summerofcode.withgoogle.com/api/program/current/project/'
-
-r = requests.get(url)
-data = r.json()
 
 name = []
 org = []
 project = []
 
-data = data['results']
+i = 1
+while(True):
+    params = {'page': i}
+    r = requests.get(url, params)
+    data = r.json()
+    data = data['results']
+    if len(data) == 0:
+        break
+    for item in data:
+        name.append(item['student']['display_name'])
+        org.append(item['organization']['name'])
+        project.append(item['title'])
+    i += 1
 
-for item in data:
-    name.append(item['student']['display_name'])
-    org.append(item['organization']['name'])
-    project.append(item['title'])
+dict = {'Name': name, 'Organization': org, 'Project': project}
+df = pd.DataFrame(dict)
+
+df.to_csv('gsoc2021.csv', index = False)
